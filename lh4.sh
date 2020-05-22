06da3b452430c2f487bef7ae107d421aa8b6693b
root <root@pi-star>
2020-05-22 19:44:22 -0400

    Update

--- lh4.sh
+++ lh4.sh
@@ -44,20 +44,39 @@ done <<< "$list9"
 var="${list4:0:240}"
 echo "${var}"
 echo "${var}" >> ./lh2_start.txt
-exit
 #echo "$list3"
 }
 ######################################
 
+ber3=""
 
 f1=$(ls -tr /var/log/pi-star/MMDVM* | tail -1)
+Last=$(tail -1 $f1)
+#echo "$Last"
+
+if [[ "$Last" == *"RF end"* ]]; then
+  	list1a=$(echo "$Last" | sed 's/,//g')
+#	echo "$list1a"
+	call3=$(echo "$list1a" | awk '{print $14}')
+	ber3=$(echo "$list1a" | awk '{print $21}')
+	rssi3=$(echo "$list1a" | awk '{print $23}')
+fi
+
+#sudo sed -n '/RF/p' $f1 | sed 's/,//g' | tail -1
 #echo "File: $f1"
 list1=$(sudo sed -n '/received network voice header from/p' $f1 | sed 's/,//g' | sort -k2,3 | tail -1)
+list3=$(echo "$list1" | awk '{print substr($2,6,5),substr($3,0,6),$12,$15}')
+
+#list1a=$(printf %s\n "tail -1 $f1 | grep 'RF end' | sed 's/,//g'")
+list1b=$(sudo sed -n '/RF end/p' $f1 | sed 's/,//g' | tail -1)
+
 #echo "$list1"
 list2=$(sudo sed -n '/received network end of voice transmission from/p' $f1 | sed 's/,//g' | sort -k2,3 | tail -1)
 #echo "$list2"
 list3=$(echo "$list1" | awk '{print substr($2,6,5),substr($3,0,6),$12,$15}')
+
 list4=$(echo "$list2" | awk '{print substr($2,6,5),substr($3,0,6),$14,$6,$17,$18,$20}')
+list3a=$(echo "$list1a" | awk '{print $12,$15}')
 #echo "$list3"
 #echo "$list4"
 
@@ -76,70 +95,25 @@ call="$call1"
         TS=$(echo "$list4" | awk '{print $4}')
         Sec=$(echo "$list4" | awk '{print $6}')
         PLoss=$(echo "$list4" | awk '{print $7}')
+
+
 if [ "$1" == "2" ]; then
         printf "3\n%s %s %s %s \nID:%s TG:%s TS:%s \nDur:%s PLoss:%s \n" "$DT" "$TM" "$call" "$name" "$did" "$TG" "$TS" "$Sec" "$PLoss"
 #        printf "3\n$DT $TM $call $name ID:$did TG:$TG"
 
 else
 	if [ "$call1" == "$call2" ]; then
-		printf "2\n%s %s \nID:%s TG:%s TS:%s Dur:%s PLoss:%s\n%s\n" "$call" "$name" "$did" "$TG" "$TS" "$Sec" "$PLoss" "$TG"
+		if [ -z "$ber3" ]; then
+			printf "2\n%s %s \nID:%s TG:%s TS:%s Dur:%s PLoss:%s\n%s\n" "$call" "$name" "$did" "$TG" "$TS" "$Sec" "$PLoss" "$TG"
+		else
+			printf "3\n%s  BER:%s  RSSI:%s\n", "$call3" "ber3" "rssi3"
+			ber3=""
+		fi
 	else
 		tg1=$(echo "$list3" | awk '{print $4}')
 		printf "1\n%s %s\nID:%s TG:%s\n" "$call" "$name" "$did" "$tg1"
 	fi
 fi
 
-echo ""
-
-
-#sudo sed -n '/received network end of voice transmission from/p' $f1 | sed 's/,//g' | sort -k2,3 | tail -1 | 
-#awk '{print substr($2,6,5),substr($3,0,6),$6,$14,$17,$18,$20}'
-
-
-exit
-######  Mode 1
-if [ "$2" == "1" ]; then
-		echo "Check OK 1 = $2" >> /home/pi-star/lh2_start.txt 
-
-	if [ "$1" == "1" ]; then
-		cat /home/pi-star/lhlog.txt | sed -n '1,10p;11q' | awk '{printf "%5s %5s %6s %1s %6s %5s %3s \n", $1, $2, $4, $3, $5, $6, $7}'
-	fi
-
-	if [ "$1" == "2" ]; then
-		cat /home/pi-star/lhlog.txt | sed -n '7,12p;13q' | awk '{printf "%5s %5s %-6s %1s %-6s %4s %4s\n", $1, $2, $4, $3, $5, $6, $7}'
-	fi
-	if [ "$1" == "3" ]; then
-		cat /home/pi-star/lhlog.txt | sed -n '13,18p;19q' | awk '{printf "%5s %5s %-6s %1s %-6s %4s %4s\n", $1, $2, $4, $3, $5, $6, $7}' 
-	fi
-	if [ "$1" == "4" ]; then
-		cat /home/pi-star/lhlog.txt | sed -n '19,24p;25q' | awk '{printf "%5s %5s %-6s %1s %-6s %4s %4s\n", $1, $2, $4, $3, $5, $6, $7}'
-	fi
-fi
-######  Mode 2
-
-if [ "$2" == "2" ]; then
-                echo "Check OK 2 = $2" >> /home/pi-star/lh2_start.txt
-
-	if [ "$1" == "1" ]; then
-		list9=$(cat /home/pi-star/lhlog.txt | sed -n '1,6p;7q' | awk '{print $1, $2, $4, $5}' )
-	fi
-
-	if [ "$1" == "2" ]; then
-		list9=$(cat /home/pi-star/lhlog.txt | sed -n '7,12p;13q' | awk '{print $1, $2, $4,$5}') 
-	fi
-	if [ "$1" == "3" ]; then
-		list9=$(cat /home/pi-star/lhlog.txt | sed -n '13,18p;19q' | awk '{print $1, $2, $4, $5}') 
-	fi
-	if [ "$1" == "4" ]; then
-		list9=$(cat /home/pi-star/lhlog.txt | sed -n '19,24p;25q' | awk '{print $1, $2, $4, $5}') 
-	fi
-
-	domode2
-fi
-#####  Mode 3
-
-
-if [ "$2" == "3" ]; then
-echo "1234567890123456789012345678901234567"
-fi
+#echo ""
 
