961ce70c3cf26bbd8cc36078ce28c041822501a8
root <root@pi-star>
2020-05-30 16:44:30 -0400

    update

--- lh3.sh
+++ lh3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #################################################
-#  Get Last Heard Liost from MMDVMHost Log	#
+#  Get Last Heard List from MMDVMHost Log	#
 #						#
 #						#
 #  VE3RD 			2020-05-03	#
@@ -32,11 +32,11 @@ do
 	pl=$(echo "$line" | cut -d' ' -f5)
 
 	echo "Add Call: $call" >> /home/pi-star/lh2_start.txt
-	name=$(sudo sed -n "/$call/p" /usr/local/etc/DMRIds.dat | sed -E "s/[[:space:]]+/|/g" | cut -d'|' -f3 | head -1)
-	did=$(sudo sed -n "/$call/p" /usr/local/etc/DMRIds.dat | sed -E "s/[[:space:]]+/|/g" | head -1 | cut -d'|' -f1)
+	name=$(sudo sed -n "/$call/p" /home/pi-star/SmartPhone/user.csv | sed -E "s/[[:space:]]+/|/g" | cut -d'|' -f3 | head -n1)
+	did=$(sudo sed -n "/$call/p" /home/pi-star/SmartPhone/user.csv | sed -E "s/[[:space:]]+/|/g" | head -n1 | cut -d'|' -f1)
 	
 
-	line2=$(echo "$line $name $did" | awk '{printf  "%5s %.5s %6s %7s %6s %6s\n",$1,$2,$3,$6,$4,$7,"   "}')
+	line2=$(echo "$line $name $did" | awk '{printf  "%5s %.5s %6s %7s %6s %6s\n",$1,$2,$3,$4,$5,$7,"   "}')
 #	echo "  $line2"
 	line3=${line2:0:37}
 	list4+="$line3"
@@ -47,38 +47,57 @@ echo "${var}" >> ./lh2_start.txt
 exit
 #echo "$list3"
 }
+
+function domode1 ()
+{
+line3=""
+while read -r line
+do
+	dt=$(echo "$line" | cut -d' ' -f1)
+	tm=$(echo "$line" | cut -d' ' -f2)
+	call=$(echo "$line" | cut -d' ' -f3)
+	tg=$(echo "$line" | cut -d' ' -f4)
+	ts=$(echo "$line" | cut -d' ' -f5)
+	dur=$(echo "$line" | cut -d' ' -f6)
+	pl=$(echo "$line" | cut -d' ' -f7)
+
+#	line2=$(echo "$line $name $did" | awk '{printf  "%5s %.5s %6s %7s %6s %6s\n",$1,$2,$3,$6,$4,$7,"   "}')
+	printf  "%5s %.5s %6s %6s %2s %4s %4s\n" "$dt" "$tm" "$call" "$tg" "$ts" "$dur" "$pl"
+#	echo "  $line2"
+	line3=${line2:0:37}
+	list4+="$line3"
+done <<< "$list9"
+#var="${list4:0:240}"
+#echo "${var}"
+#echo "${var}" >> ./lh2_start.txt
+exit
+#echo "$list3"
+
+}
 ######################################
-f1=$(ls -tr /var/log/pi-star/MMDVM* | tail -1)
+f1=$(ls -tr /var/log/pi-star/MMDVM* | tail -n1)
 list1=$(sudo sed -n '/received network end of voice transmission from/p' $f1 | sed 's/,//g' | sort -r -k14,14 -k2,3)
 
-f2=$(ls -tr /var/log/pi-star/MMDVM* | head -1)
+f2=$(ls -tr /var/log/pi-star/MMDVM* | head -n1)
 list2=$(sudo sed -n '/received network end of voice transmission from/p' $f2 | sed 's/,//g' | sort -r -k14,14 -k2,3)
 
 #echo "$f1" #Newest
 #echo "$f2"
-list3=$(echo "$list1" | awk '{print substr($2,6,5),substr($3,0,6),$6,$14,$17,$18,$20}')
-list4=$(echo "$list2" | awk '{print substr($2,6,5),substr($3,0,6),$6,$14,$17,$18,$20}')
+list3=$(echo "$list1" | awk '{print substr($2,6,5),substr($3,0,6),$14,$6,$17,$18,$20}' | sort -r -k3,3 -k1,2)
+list4=$(echo "$list2" | awk '{print substr($2,6,5),substr($3,0,6),$14,$6,$17,$18,$20}' | sort -r -k3,2 -k1,2)
+
 list1=""
 list2=""
 
-list6=$(echo "$list3" | awk '$4!=savestr {print $1,$2,$3,$4,$5,$6,$7; savestr=$4}' | sort -r -k4 | head -24)
+list6=$(echo "$list3" | awk '$3!=savestr {print $1,$2,$3,$4,$5,$6,$7; savestr=$3}' | sort  -k1,2 | tail -n 24 | sort -r -k1,2)
 
 lcnt=$(echo "$list6" | wc -l)
 
 if [ $lcnt -lt 20 ]; then
-	list7=$(echo "$list4" | awk '$4!=savestr {print $1,$2,$3,$4,$5,$6,$7; savestr=$4}' | sort -r -k4 | head -24)
+	list7=$(echo "$list4" | awk '$3!=savestr {print $1,$2,$3,$4,$5,$6,$7; savestr=$3}' | sort k1,2 | tail -n 24 | sort -r -k1,2)
 	list6+="$list7"
 fi
 
-list4=""
-
-list8=$(echo "$list6" | sort -r -k4 | awk '$4!=savestr {print $1,$2,$3,$4,$5,$6,$7; savestr=$4'} | sort -r -k1,2 | head -24)
-echo "$list8" > /home/pi-star/lhlog.txt
-
-
-#list7=$(echo "$list3" | awk 'BEGIN {format = "%-6s %-6s %-10s %-10s %-10s %-10s %s\n" } $4!=savestr  {printf  $1, $2, $3, $4, $5, $6, $7; savestr=$4}' | sort -r -k1,2 | head -24)
-#list7=$(echo "$list3" | awk '{printf "%-6s %-6s %-10s %-10s %-10s %-10s %s\n", $1, $2, $3, $4, $5, $6, $7}' | sort -r -k1,2 | head -24)
-#echo "$list7"
 list3=""
 
 n=0 
@@ -89,8 +108,15 @@ if [ "$2" == "1" ]; then
 		echo "Check OK 1 = $2" >> /home/pi-star/lh2_start.txt 
 
 	if [ "$1" == "1" ]; then
-		cat /home/pi-star/lhlog.txt | sed -n '1,15p;16q' | awk '{printf "%5s %5s %8s %2s %7s %5s %3s \n", $1, $2, $4, $3, $5, $6, $7}'
-	fi
+		list9=$(echo "$list6" | awk '{print $1, $2, $3, $4, $5, $6, $7,"\r"}' | sed -n '1,12p;13q')
+	#echo $list9"
+	fi 
+	if [ "$1" == "2" ]; then
+		list9=$(echo "$list6" | awk '{print $1, $2, $3, $4, $5, $6, $7,"\r"}' | sed -n '12,24p;25q')
+	echo #$list9"
+	fi 
+		domode1
+
 fi
 ######  Mode 2
 
